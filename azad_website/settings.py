42ce534b3a818b070b155741bb05e7f25037cf4d
harsh <harsh.gupta@ylytic.com>
2023-12-30 02:19:11 +0530

    update

--- azad_website/settings.py
+++ azad_website/settings.py
@@ -146,7 +146,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# For local, is should be /static/
+# For production, it should be /azad/static/
+STATIC_URL = '/azad/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'azad_website'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
