050ad68f3d5e953ec5870bd34c984fff24febf1a
harsh <harsh.gupta@ylytic.com>
2023-12-30 17:23:24 +0530

    update

--- azad_website/settings.py
+++ azad_website/settings.py
@@ -174,6 +174,7 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend'
 ]
+
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
