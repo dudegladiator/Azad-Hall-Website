52fb90066478fe7dd2dc9f1ce2c272085b5db3f5
poonia3 <pooniakushagra20@gmail.com>
2024-12-07 15:52:29 +0530

    sMall fIx

--- azad_website/azad/assets/js/complain.js
+++
@@ -1,7 +0,0 @@
-let complains = document.querySelectorAll(".complain");
-complains.forEach((complain)=>{
-    if(complain.innerHTML.length>70){
-        complain.innerHTML = (complain.innerHTML.substring(0, 50)+" ... "+complain.innerHTML.substring(complain.innerHTML.length-1, complain.innerHTML.length-40));
-    }
-    
-})
