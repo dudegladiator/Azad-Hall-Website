101ae281b80f291d61ffb401c6163b0fa8784d6c
ArnabDas9039 <arnabdas.9039@gmail.com>
2025-01-11 21:12:54 +0530

    library features checked 21:13

--- azad_website/azad/forms.py
+++ azad_website/azad/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.core.validators import RegexValidator
 from .models import Contact, Comment, complaints
 
 
@@ -15,33 +14,6 @@ class ContactForm(forms.ModelForm):
             "msg": forms.Textarea(attrs={"class": "form-control"}),
         }
         widget = forms.FileInput(attrs={"class": "form-control"})
-
-
-class UserForm(forms.Form):
-    name = forms.CharField(
-        label="Name",
-        max_length=100,
-        widget=forms.TextInput(attrs={"placeholder": "Enter your full name"}),
-    )
-    phone_number = forms.CharField(
-        label="Phone Number",
-        max_length=15,
-        validators=[
-            RegexValidator(
-                regex=r"^\+?1?\d{9,15}$",
-                message="Phone number must be in the format: '+999999999'. Up to 15 digits allowed.",
-            ),
-        ],
-        widget=forms.TextInput(
-            attrs={"placeholder": "Enter phone number with country code"}
-        ),
-    )
-    details = forms.CharField(
-        label="Details",
-        widget=forms.Textarea(
-            attrs={"placeholder": "Enter additional details here", "rows": 5}
-        ),
-    )
 
 
 class CommentForm(forms.ModelForm):
