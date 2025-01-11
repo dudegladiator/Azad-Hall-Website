91666af4ca37bb407c9dcb49ee0510bb2e66406f
Arka das <165567289+1arka02@users.noreply.github.com>
2025-01-12 00:55:10 +0530

    Forms.py

--- azad_website/azad/forms.py
+++ azad_website/azad/forms.py
@@ -1,6 +1,5 @@
-
 from django import forms
-from .models import *
+from .models import Contact, Comment, complaints
 
 
 class ContactForm(forms.ModelForm):
@@ -9,17 +8,32 @@ class ContactForm(forms.ModelForm):
         fields = "__all__"
 
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'email': forms.TextInput(attrs={'class': 'form-control'}),
-            'subject': forms.TextInput(attrs={'class': 'form-control'}),
-            'msg': forms.Textarea(attrs={'class': 'form-control'}),
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "email": forms.TextInput(attrs={"class": "form-control"}),
+            "subject": forms.TextInput(attrs={"class": "form-control"}),
+            "msg": forms.Textarea(attrs={"class": "form-control"}),
         }
-        widget = forms.FileInput(attrs={'class': 'form-control'})
+        widget = forms.FileInput(attrs={"class": "form-control"})
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('event', 'name', 'email', 'body')
+        fields = ["event", "name", "email", "body"]
 
 
+class ComplaintForm(forms.ModelForm):
+    upload_image = forms.FileField(required=False)
+
+    class Meta:
+        model = complaints
+        fields = [
+            "name",
+            "roll_no",
+            "category",
+            "email",
+            "contact_no",
+            "room_no",
+            "complain",
+            "upload_image",
+        ]
